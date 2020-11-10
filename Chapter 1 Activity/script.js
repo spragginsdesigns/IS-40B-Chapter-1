7edf2218a8f16b79a667c417f11569b1c395d5f0
Austin <43624199+Atmosphere9999@users.noreply.github.com>
2020-11-09 16:16:35 -0800

    Add files via upload
    Assignment Details: 
    Create a basic webpage that uses JavaScript to calculate the volume of 5 identical boxes.  Have variables to hold the length, width and height of a box.  Besides the calculations, the JavaScript code should also write to the webpage the volume of a single box as well as the total volume of all 5 boxes combined.  Make sure to give the user the info in complete sentences so they understand what the numbers represent.
    
    For this lab and all future labs and projects take print screens of both your code and the code running in the browser window and paste them into a Word Document.  Turn the Word Document into Canvas.

---
+++ Chapter 1 Activity/script.js
@@ -0,0 +1,7 @@
+/* Code For 5 Identical Boxes and Volume Of 1 Box */
+var length = 5;
+var width = 6;
+var height = 8;
+var totalvolume = length * width * height;
+document.writeln("This Box's Volume is: " + totalvolume +  "<br/>");
+document.writeln("This Is The Volume Of 5 Identical Boxes: " + totalvolume * 5);
